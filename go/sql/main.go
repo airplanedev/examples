45636cfb742c87a3c5f6e832f0a1bd35d533e22f
Colin King <me@colinking.co>
2021-04-02 09:50:58 -0500

    copy

--- go/sql/main.go
+++ go/sql/main.go
@@ -18,10 +18,18 @@ func main() {
 }
 
 type Parameters struct {
-	Query         string `json:"query"`
-	Driver        string `json:"driver"`
-	Source        string `json:"source"`
-	S3URL         string `json:"s3URL"`
+	// The SQL query to execute.
+	SQL string `json:"sql"`
+	// The type of SQL database to run the query on. Currently this can only be "postgres".
+	Driver string `json:"driver"`
+	// Where to get the DB DSN from. Defaults to "env", which pulls it from an `AIRPLANE_DSN`
+	// environment variable. Can also be set to "s3" to pull the DSN from an s3 bucket.
+	Source string `json:"source"`
+	// If source="s3", this should be the full URL (s3://...) of an S3 document.
+	S3URL string `json:"s3URL"`
+	// If source="s3", this can be used to fetch a specific element from the above S3 document
+	// where the contents are parsed as JSON. If not set, defaults to treating the document's
+	// contents as the DSN.
 	S3JSONPointer string `json:"s3JSONPointer"`
 }
 
@@ -59,7 +67,7 @@ func run(ctx context.Context) error {
 		defer db.Close()
 
 		// Since we are wrapping the user-provided query, we need to remove the semicolon, if present.
-		innerQuery := strings.TrimSuffix(strings.TrimSpace(params.Query), ";")
+		innerQuery := strings.TrimSuffix(strings.TrimSpace(params.SQL), ";")
 		rows, err := db.Queryx(fmt.Sprintf(`SELECT row_to_json(t) FROM (%s) t`, innerQuery))
 		if err != nil {
 			return errors.Wrap(err, "running query on db")

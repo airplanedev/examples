9ce10c60e8c8edd0dda1d13d1377e92b3c147692
Colin King <me@colinking.co>
2021-05-28 14:06:50 -1000

    update examples

--- node/http-typescript/somejs/hello.js
+++
@@ -1,9 +0,0 @@
-import cowsay from "cowsay";
-
-export const hello = () => {
-  return "hello!";
-};
-
-export const say = (msg) => {
-  console.log(cowsay.say({ text: msg }));
-};
--- node/http-typescript/somets/world.ts
+++
@@ -1,5 +0,0 @@
-import cowsay from "cowsay";
-
-export const sayButWithTypes = (msg: string) => {
-  console.log(cowsay.say({ text: msg }));
-};
